4b8227620fa1921aea7d4e52e5d325cbba2e98a5
hugosmhd <hugo.kio40@gmail.com>
2024-02-17 13:22:29 -0600

    Agregando Systemtap

--- Practica 1/child.c
+++ Practica 1/child.c
@@ -9,7 +9,7 @@
 #define LINE_LENGTH 8
 
 void random_calls(int fd) {
-    srand(time(NULL));
+    // srand(time(NULL));
     int operation = rand() % 3; // Operación aleatoria (0: Write, 1: Read, 2: Seek)
     char buffer[LINE_LENGTH + 1]; // Buffer para almacenar datos
 
@@ -17,29 +17,28 @@ void random_calls(int fd) {
         for (int i = 0; i < LINE_LENGTH; i++) {
             buffer[i] = 'A' + (rand() % 26);
         }
-        buffer[LINE_LENGTH] = '\n';
         write(fd, buffer, strlen(buffer));
     } else if (operation == 1) {
-            read(fd, buffer, LINE_LENGTH);
+        read(fd, buffer, LINE_LENGTH);
     } else if (operation == 2) {
-            lseek(fd, 0, SEEK_SET); 
+        lseek(fd, 0, SEEK_SET); 
     }
 }
 
 int main() {
-    int fd = open(FILENAME, O_CREAT | O_RDWR, 0644);
-    if (fd == -1) {
-        perror("Error al abrir el archivo");
-        exit(EXIT_FAILURE);
-    }
+    printf("Hola hijo: %d", getpid());
+    int fd = open(FILENAME, O_APPEND | O_RDWR, 0644);
 
     srand(getpid());
-    printf("Proceso hijo\n");
     while (1) {
+        if (fd == -1) {
+            perror("Error al abrir el archivo");
+            exit(EXIT_FAILURE);
+        }
         random_calls(fd);
         sleep(rand() % 3 + 1);
     }
-
     close(fd);
+
     return 0;
 }
--- Practica 1/parent.c
+++ Practica 1/parent.c
@@ -11,62 +11,50 @@
 #include <signal.h>
 #include <time.h>
 
+#define FILENAME "practica1.txt"
 #define NUM_CHILD 2
 
 volatile sig_atomic_t stop = 0;
 
-int main(){
+int main() {
 
     printf("** proc. PID = %d comienza ** \n", getpid());
-    int pid;
+    int pid_child[NUM_CHILD];
     int status;
-
+    int fd = open(FILENAME, O_CREAT | O_RDWR | O_TRUNC, 0644);
+    close(fd);
+    
     for (int i = 0; i < NUM_CHILD; i++) {
-        pid = fork();
-        if (pid == -1) {
+        pid_child[i] = fork();
+        if (pid_child[i] == -1) {
             perror("Error en fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // printf("Proceso hijo\n");
+        } else if (pid_child[i] == 0) {
+            printf("Proceso hijo PID:  %d\n", getpid());
             execl("./child.bin", "child.bin", NULL);
             perror("Error al ejecutar el proceso hijo");
             exit(EXIT_FAILURE);
         } else {
             printf("Proceso padre\n");
+            
         }
     }
+
+    // Esperar un segundo antes de ejecutar SystemTap
+    // sleep(1);
+    char command[100];
+    printf("Proceso hola: %d, %d\n", pid_child[0], pid_child[1]);
+    sprintf(command, "%s %d %d %s", "sudo stap trace.stp ", pid_child[0], pid_child[1], " > syscalls.log");
+    system(command);
 
     while (!stop) {
         sleep(1); // Espera un segundo
     }
 
     for (int i = 0; i < NUM_CHILD; i++) {
-        pid = wait(&status);
-        printf("Padre ha terminado PID = %d, hijo de PID = %d terminado, estado = %d \n", getpid(), pid, WEXITSTATUS(status));
+        pid_child[i] = wait(&status);
+        printf("Padre ha terminado PID = %d, hijo de PID = %d terminado, estado = %d \n", getpid(), pid_child[i], WEXITSTATUS(status));
     }    
-
-    // for (int i = 0; i < 2; i++) {
-    //     int pid1 = fork();
-    //     if (pid1 == -1) {
-    //         perror("Error en fork");
-    //         exit(EXIT_FAILURE);
-    //     } else if (pid1 == 0) {
-    //         printf("Proceso hijo\n");
-    //         monitor_syscalls();
-    //         sleep(3);
-    //         exit(EXIT_SUCCESS);
-    //     } else {
-    //         printf("Proceso padre\n");
-    //         int status;
-    //         wait(&status);
-
-    //         if(WIFEXITED(status)){
-    //             printf("El proceso hijo termino con estado: %d\n", WEXITSTATUS(status));
-    //         } else {
-    //             printf("Ocurrio un error al terminar el proceso hijo\n");
-    //         }
-    //     }
-    // }
 
     return 0;
 }
